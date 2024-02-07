73a513196ca95d0f81446e6af3c6c2cec780e8e2
ENGINEER MENGE <157677665+victormenge@users.noreply.github.com>
2024-02-07 21:54:43 +0300

    assignment.c

--- assignment.c
+++ assignment.c
@@ -3,15 +3,15 @@
 
 int main()
 {
-    int a, b, c;
+    int a, b, c d;
     printf("Enter the first value : ");
     scanf("%d" , &a);
     printf("Enter the second value : ");
     scanf("%d" , &b);
     printf("Enter the third value : ");
     scanf("%d" , &c);
-    c = a + b + c;
-    printf("%d + %d = %d\n" , a, b, c, c);
+    d = a + b + c;
+    printf("%d + %d = %d\n" , a, b, c, d);
     return 0;
 
    }
